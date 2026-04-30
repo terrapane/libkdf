01e5fd4a39204d9165a5fb98031ab1b0f228d6ce
Paul E. Jones <paulej@packetizer.com>
2026-04-30 10:24:49 -0400

    Changed how the key vector is zeroed

--- test/hkdf/test_hkdf.cpp
+++ test/hkdf/test_hkdf.cpp
@@ -391,7 +391,7 @@ STF_TEST(PBKDF2, RFC5869_A5)
 
     // Call Expand again (repeated calls with the same "info" should yield the
     // same output key)
-    std::ranges::fill(key, 0x00);
+    key = {};
     result = hkdf.Expand(info, key);
 
     STF_ASSERT_EQ(key.data(), result.data());
